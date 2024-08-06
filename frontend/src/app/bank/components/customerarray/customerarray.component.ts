382cf9254ecdcb85f6321d35cc98e1b691972d6e
username <wecp-dummy@gmail.com>
2024-08-06 04:58:38 +0000

    Day 25

--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import { of } from 'rxjs';
+// import { Customer } from '../bank/types/Customer';
 import { CustomerTS } from '../../types/tstypes/Customerts';
-
+ 
 @Component({
   selector: 'app-customerarray',
-  // standalone: true,
-  // imports: [],
+ 
   templateUrl: './customerarray.component.html',
   styleUrls: ['./customerarray.component.css']
 })
 export class CustomerarrayComponent {
-  
-  customer: CustomerTS=new CustomerTS("John Doe", "john@example.com", "john_doe", "password123", "User", "1")
-  customer2: CustomerTS=new CustomerTS("John Doe1", "john1@example.com", "john_doe1", "password123", "Admin", "2")
-
-  customers: CustomerTS[] = [this.customer,this.customer2];
-  
-
+  customers: CustomerTS[] = [
+    new CustomerTS("John Doe", "john@example.com", "john_doe", "password123", "User", "1"),
+    new CustomerTS("John Doe1", "john1@example.com", "john_doe", "password123", "Admin", "2")
+   ]
 }
-
--- frontend/src/app/bank/types/Customer.ts
+++ frontend/src/app/bank/types/Customer.ts
@@ -5,7 +5,7 @@ export class Customer {
     password:string;
     username:string;
     role?:string;
- 
+  
       constructor(data:any) {
           this.customerId = data.customerId;
           this.name = data.name;
@@ -14,7 +14,7 @@ export class Customer {
           this.password = data.password;
           this.role = data.role;
       }
- 
+  
       displayInfo() {
           console.log(`Customer ID: ${this.customerId}`);
           console.log(`Name: ${this.name}`);
@@ -25,3 +25,9 @@ export class Customer {
           console.log("------");
       }
   }
+  
+  
+  
+  
+  
+  
